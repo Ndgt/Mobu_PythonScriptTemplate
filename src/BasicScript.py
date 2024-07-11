ad38e1e42179954550f064a5cc1b650d44f30a38
Ndgt <nadesubber2@gmail.com>
2024-07-12 08:06:11 +0900

    add BasicScript.py

---
+++ src/BasicScript.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from pyfbsdk import*
+from pyfbsdk_additions import*
+
+def main():
+    somefunction()
+
+def somefunction():
+
+if __name__ in ("__main__", "builtins"):
+    main()  
